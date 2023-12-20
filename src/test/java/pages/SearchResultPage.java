0e90c3ff78f3a1685948af3cbdc2f9c9e0d7b5a1
A2ZTestAutomation <corpdevops@gmail.com>
2023-12-20 11:02:48 +0530

    BDD with new framework POM

---
+++ src/test/java/pages/SearchResultPage.java
@@ -0,0 +1,33 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import base.TestBaseOld;
+
+public class SearchResultPage{
+	
+	@FindBy(css = "ul li.preview")
+	WebElement itemIcon;
+	
+	@FindBy(xpath = "//button[contains(text(), 'Add to cart')]")
+	WebElement addToBtn;
+	
+	public SearchResultPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void viewItemDetail() {
+		itemIcon.click();
+	}
+	public boolean isItemListed() {
+		return itemIcon.isDisplayed();
+	}
+	public void addToCart() {
+		addToBtn.click();
+	}
+	
+
+}
